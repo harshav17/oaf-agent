252551c74fc9b59db1881691758bb937be4924ef
harsha <harshav17@gmail.com>
2023-10-04 21:39:36 -0700

    use openai 4

--- src/oaf.ts
+++ src/oaf.ts
@@ -1,7 +1,8 @@
-import { ChatCompletionRequestMessageRoleEnum, ChatCompletionRequestMessage, Configuration, OpenAIApi, ChatCompletionFunctions } from "openai";
+import OpenAI, { ClientOptions } from "openai";
 import { IncomingMessage } from "http";
 import { Writable } from "node:stream";
 import debugModule from "debug";
+import { ChatCompletionMessageParam, Chat, ChatCompletionCreateParams } from "openai/resources/chat";
 
 const decoder = new TextDecoder("utf-8");
 const debug = debugModule("oaf:core");
@@ -13,100 +14,67 @@ const DEFAULT_TEMPERATURE = 0.5;
 export type OafOptions = {
     finString: string;
     funcs?: any;
-    funcDescs?: ChatCompletionFunctions[];
+    funcDescs?: ChatCompletionCreateParams.Function[];
     model?: string;
     max_tokens?: number;
     tempature?: number;
     shouldRecurse?: boolean;
 };
-export async function callOaf(messages: ChatCompletionRequestMessage[], res: Writable, configuration: Configuration, options: OafOptions) {
-    const openai = new OpenAIApi(configuration);
+export async function callOaf(messages: ChatCompletionMessageParam[], res: Writable, clientOpts: ClientOptions, options: OafOptions) {
+    const openai = new OpenAI(clientOpts);
     debug("Started oaf with messages: %o", messages);
     await callOafHelper(messages, res, openai, options);
 }
 
-async function callOafHelper(messages: ChatCompletionRequestMessage[], res: Writable, openai: OpenAIApi, options: OafOptions, isRecursive: boolean = false, ) {
+async function callOafHelper(messages: ChatCompletionMessageParam[], res: Writable, openai: OpenAI, options: OafOptions, isRecursive: boolean = false, ) {
     const { finString, model, max_tokens, tempature, funcDescs, funcs, shouldRecurse } = options;
     let functionCalls: any[] = [];
     let currentFunctionCallName = "";
     let currentMessageFromGPT = "";
     debug("Started oaf with funcs: %o", funcDescs);
     try {
-        const completion = await openai.createChatCompletion(
-            {
-                model: model || DEFAULT_MODEL, // TODO: Make this configurable
-                messages: messages,
-                stream: true,
-                max_tokens: max_tokens || DEFAULT_MAX_TOKENS,
-                temperature: tempature || DEFAULT_TEMPERATURE,
-                functions: funcDescs ? funcDescs : undefined,
-                function_call: funcDescs ? "auto" : undefined,
-            },
-            { responseType: "stream" },
-        );
-
-        const stream = completion.data as unknown as IncomingMessage;
-
-        let partialLine = "";
-        for await (const chunk of stream) {
-            const value = decoder.decode(chunk);
-            const lines = value.split("\n");
-            lines[0] = partialLine + lines[0]; // prepend the leftover from the last chunk to the first line
-            partialLine = lines.pop() || ""; // save the last line as it may be incomplete
-
-            const parsedLines: any = lines
-                .map((line) => line.replace("data: ", "")) // Remove the "data: " prefix
-                .filter((line) => line !== "" && line !== "[DONE]") // Remove empty lines and "[DONE]"
-                .map((line) => {
-                    try {
-                        return JSON.parse(line);
-                    } catch(e) {
-                        debug("Error parsing line: %s", line);
-                        debug("Error details: %o", e);
-                        return null;
-                    }
-                })
-                .filter(line => line !== null); // Remove null lines
-
-            for (const parsedLine of parsedLines) {
-                const { error, choices } = parsedLine;
-                if (error) {
-                    // TODO send error back to client
-                    res.end();
-                    break;
+        const stream = await openai.chat.completions.create({
+            model:  model || DEFAULT_MODEL,
+            messages: messages,
+            stream: true,
+            functions: funcDescs ? funcDescs : undefined,
+            function_call: funcDescs ? "auto" : undefined,
+            temperature: tempature || DEFAULT_TEMPERATURE,
+            max_tokens: max_tokens || DEFAULT_MAX_TOKENS,
+        });
+        for await (const part of stream) {
+            const { finish_reason } = part.choices[0];
+            const { content, function_call } = part.choices[0].delta;
+            if (finish_reason) {
+                break;
+            } else if (content) {
+                currentMessageFromGPT += content;
+                res.write(content);
+            } else if (function_call) {
+                if (function_call.name) {
+                    res.write(function_call.name);
+                    currentFunctionCallName = function_call.name;
                 }
-                const { content, function_call, finish_reason } = choices[0].delta;
-                if (finish_reason) {
-                    break;
-                } else if (content) {
-                    currentMessageFromGPT += content;
-                    res.write(content);
-                } else if (function_call) {
-                    if (function_call.name) {
-                        res.write(function_call.name);
-                        currentFunctionCallName = function_call.name;
-                    }
-                    if (function_call.arguments) {
-                        res.write(function_call.arguments);
-                        let existingFunctionCall = functionCalls.find((call) => call.name === currentFunctionCallName);
-                        if (existingFunctionCall) {
-                            // If a function call with the same name already exists, append the args
-                            existingFunctionCall.argsString += function_call.arguments;
-                        } else {
-                            // Otherwise, create a new function call
-                            functionCalls.push({
-                                name: currentFunctionCallName,
-                                argsString: function_call.arguments || {},
-                            });
-                        }
+                if (function_call.arguments) {
+                    res.write(function_call.arguments);
+                    let existingFunctionCall = functionCalls.find((call) => call.name === currentFunctionCallName);
+                    if (existingFunctionCall) {
+                        // If a function call with the same name already exists, append the args
+                        existingFunctionCall.argsString += function_call.arguments;
+                    } else {
+                        // Otherwise, create a new function call
+                        functionCalls.push({
+                            name: currentFunctionCallName,
+                            argsString: function_call.arguments || {},
+                        });
                     }
                 }
-            }
+            }    
         }
 
         if (currentMessageFromGPT.trim().length > 0) {
             messages.push({
-                role: ChatCompletionRequestMessageRoleEnum.Assistant,
+                role: "assistant",
                 content: currentMessageFromGPT,
             });
         }
@@ -119,7 +87,7 @@ async function callOafHelper(messages: ChatCompletionRequestMessage[], res: Writ
                 const funcRes = await func(args);
 
                 messages.push({
-                    role: ChatCompletionRequestMessageRoleEnum.Assistant,
+                    role: "assistant",
                     content: "",
                     function_call: {
                         name: functionCall.name,
@@ -127,7 +95,7 @@ async function callOafHelper(messages: ChatCompletionRequestMessage[], res: Writ
                     },
                 });
                 messages.push({
-                    role: ChatCompletionRequestMessageRoleEnum.Function,
+                    role: "function",
                     content: JSON.stringify(funcRes),
                     name: functionCall.name,
                 });
